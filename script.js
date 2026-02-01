0bdeea962454c9fa97ab6160757f4dec6f294db1
manpreetahluwalia575-a11y <manpreetahluwalia575@gmail.com>
2026-02-01 13:07:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+const noBtn = document.getElementById("no");
+const yesBtn = document.getElementById("yes");
+const question = document.getElementById("question");
+const subtext = document.getElementById("subtext");
+const img = document.getElementById("img");
+
+let noCount = 0;
+
+const savageTexts = [
+  "Nice try 😏",
+  "Bubu, not happening 😂",
+  "You sure? 😌",
+  "Wrong button 😈",
+  "Come on Bubu 💖",
+  "JUST CLICK YES 😤",
+  "I’ll wait 😎"
+];
+
+noBtn.addEventListener("mouseover", () => {
+  noCount++;
+
+  const x = Math.random() * 220 - 110;
+  const y = Math.random() * 180 - 90;
+  noBtn.style.transform = `translate(${x}px, ${y}px)`;
+
+  subtext.innerText = savageTexts[Math.min(noCount, savageTexts.length - 1)];
+});
+
+noBtn.addEventListener("click", () => {
+  subtext.innerText = "HAHA you can’t 😜";
+});
+
+yesBtn.addEventListener("click", () => {
+  question.innerHTML = "YAYYY 💖🥰";
+  subtext.innerHTML = "Bubu said YES 😭✨";
+  img.src = "https://i.imgur.com/VAk1zGk.png";
+  noBtn.style.display = "none";
+  yesBtn.style.display = "none";
+});
